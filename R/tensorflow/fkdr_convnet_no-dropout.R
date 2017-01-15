589ffeeef1c730fc8fc1cadea21e320c5b2fded8
henry-p <henry.perschk@gmail.com>
2017-01-15 20:40:30 +0100

    .

--- R/tensorflow/fkdr_convnet_no-dropout.R
+++ R/tensorflow/fkdr_convnet_no-dropout.R
@@ -25,7 +25,7 @@ test.y = (test.y - 48) / 48
 
 # Parameters
 learning_rate = 0.001
-training_epochs = 1000L
+training_epochs = 25L
 batch_size = 50L
 display_step = 1L
 
@@ -76,17 +76,18 @@ h_conv2 <- tf$nn$relu(conv2d(h_pool1, W_conv2) + b_conv2)
 ## Fourth layer (pooling)
 h_pool2 <- max_pool_2x2(h_conv2)
 ## Fifth layer (densely connected)
-W_fc1 <- weight_variable(shape(24L * 24L * 64L, 1024L))
+W_fc1 <- weight_variable(shape(36864L, 1024L))
 b_fc1 <- bias_variable(shape(1024L))
 h_pool2_flat <- tf$reshape(h_pool2, shape(-1L, 24L * 24L * 64L))
 h_fc1 <- tf$nn$relu(tf$matmul(h_pool2_flat, W_fc1) + b_fc1)
-# Dropout
-keep_prob <- tf$placeholder(tf$float32)
-h_fc1_drop <- tf$nn$dropout(h_fc1, keep_prob)
+## Dropout
+# keep_prob <- tf$placeholder(tf$float32)
+# h_fc1_drop <- tf$nn$dropout(h_fc1, keep_prob)
 ## Sixth layer (readout)
 W_fc2 <- weight_variable(shape(1024L, 30L))
 b_fc2 <- bias_variable(shape(30L))
-y_conv <- tf$matmul(h_fc1_drop, W_fc2) + b_fc2
+# y_conv <- tf$matmul(h_fc1_drop, W_fc2) + b_fc2
+y_conv <- tf$matmul(h_fc1, W_fc2) + b_fc2
 
 # Define loss and optimizer
 cost = tf$reduce_mean(tf$square(y_conv - y))
@@ -115,32 +116,32 @@ for(epoch in seq_len(training_epochs)) {
   for(batchNr in seq_len(numberOfBatches)) {
     rowIndices = nextBatchIndices(shuffledIndices, batchNr, batch_size)
 
-    train_accuracy <- sess$run(accuracy, feed_dict = dict(x = train.x[rowIndices, ], y = train.y[rowIndices, ], keep_prob = 1.0))
+    train_accuracy <- sess$run(accuracy, feed_dict = dict(x = train.x[rowIndices, ], y = train.y[rowIndices, ]))
     cat(sprintf("Epoch: %d | Batch: %d/%d | Training RMSE: %g\n", epoch, batchNr, numberOfBatches, train_accuracy))
 
-    sess$run(optimizer, feed_dict = dict(x = train.x[rowIndices, ], y = train.y[rowIndices, ], keep_prob = 0.75))
+    sess$run(optimizer, feed_dict = dict(x = train.x[rowIndices, ], y = train.y[rowIndices, ]))
   }
 }
 
-test_accuracy <- sess$run(accuracy, feed_dict = dict(x = test.x, y = test.y, keep_prob = 1.0))
+test_accuracy <- sess$run(accuracy, feed_dict = dict(x = test.x, y = test.y))
 cat(sprintf("Test RMSE: %g", test_accuracy))
 
 # Plot on first test image
 data = test.x * 255
-pred = sess$run(y_conv, feed_dict = dict(x = test.x, keep_prob = 1.0)) * 48 + 48
+pred = sess$run(y_conv, feed_dict = dict(x = test.x)) * 48 + 48
 plotFacialKeypoints(data, 1, pred)
 
 # # Save data
 # saver <- tf$train$Saver()
-# data_file <- saver$save(sess, paste0("/Users/henry/fkdr_submissions/", "fkdr_cnn_203epochs_0.75dropoutrate.ckpt"))
+# data_file <- saver$save(sess, paste0("/Users/henry/fkdr_submissions/", "fkdr_cnn_25epochs.ckpt"))
 #
 # # Restore Data
 # Watch out to restore the correct checkpoint file!!!
-sess = tf$Session()
-restorer = tf$train$import_meta_graph(paste0("/Users/henry/fkdr_submissions/", "fkdr_cnn_203epochs_0.75dropoutrate.ckpt.meta"))
-restorer$restore(sess, tf$train$latest_checkpoint("/Users/henry/fkdr_submissions/"))
+# sess = tf$Session()
+# restorer = tf$train$import_meta_graph(paste0("/Users/henry/fkdr_submissions/", "fkdr_cnn????.ckpt.meta"))
+# restorer$restore(sess, tf$train$latest_checkpoint("/Users/henry/fkdr_submissions/"))
 #
 # Make submission file
 data = d.test$Image / 255
-pred = sess$run(y_conv, feed_dict = dict(x = data, keep_prob = 1.0)) * 48 + 48
+pred = sess$run(y_conv, feed_dict = dict(x = data)) * 48 + 48
 writeSubmissionFile(predictions = pred, "/Users/henry/fkdr_submissions/")
